367ecb85dd0e91cf10ee8e47350d9c545bdda627
nico <ndabd001@fiu.edu>
2024-04-02 16:52:37 -0400

    practiced jsx multi option dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from "react";
 
 function App() {
+
+  const inputRef = useRef(null);
+
+  const onClick = () => {
+    console.log(inputRef.current.value);
+  }
+
+  let fruits = [
+    { label: "Apple", value: "🍎" },
+    { label: "Banana", value: "🍌" },
+    { label: "Orange", value: "🍊" }
+  ]
+
+  let options = [
+    { label: "blah", value: "blah" },
+    { label: "garbage", value: "ridiculous" },
+    { label: "jgwentworth", value: "guitar" }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input type="text" placeholder="Example" ref={inputRef} onChange={onClick}/>
+        <select>
+          <option value={"Select a fruit"}>Select a Fruit</option>
+          {fruits.map((fruits) => <option value={fruits.value}>{fruits.label}</option>)}
+        </select>
+
+        <select>
+          <option value="Select">Select</option>
+          {options.map((options) => <option value={options.value}>{options.label}</option>)}
+        </select>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
